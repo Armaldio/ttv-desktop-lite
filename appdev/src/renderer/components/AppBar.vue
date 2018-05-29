d715d95cf6be2d8a152f172a6d5728658c3f8791
TheRealDannyyy <dannyyy55fly@googlemail.com>
2018-05-29 20:38:17 +0200

    Appbar Styling Update

--- appdev/src/renderer/components/AppBar.vue
+++ appdev/src/renderer/components/AppBar.vue
@@ -1,10 +1,12 @@
 <template>
-    <v-system-bar style="-webkit-app-region: drag" window dark>
+    <v-system-bar style="-webkit-app-region: drag; height: 25px; background-color: #2c2541;" window dark>
         <v-icon>menu</v-icon>
-        <v-spacer style="-webkit-app-region: drag"></v-spacer>
+		<v-spacer style="-webkit-app-region: drag;"></v-spacer>
+		<v-text style="-webkit-app-region: drag;">Twitchy Desktop Light</v-text>
+		<v-spacer style="-webkit-app-region: drag;"></v-spacer>
         <v-icon @click="$electron.remote.getCurrentWindow().minimize()">remove</v-icon>
         <v-icon @click="toggleRestore">check_box_outline_blank</v-icon>
-        <v-icon @click="$electron.remote.app.quit()">close</v-icon>
+        <v-icon @click="$electron.remote.app.quit()" class="close">close</v-icon>
     </v-system-bar>
 </template>
 
@@ -24,9 +26,29 @@
 </script>
 
 <style scoped>
-    .icon {
+    .material-icons {
         -webkit-app-region: no-drag;
 		-webkit-user-select: none !important;
         cursor: pointer;
+		height: 25px;
+		padding-top: 3px;
+		padding-left: 3px;
+		padding-right: 3px;
+		background-color: rgba(100, 100, 100, 0.0);
+		transition: border-bottom 100ms ease-in-out;
+		border-bottom: 1px solid hsla(0,0%,100%,.0);
+    }
+    .material-icons:hover {
+		border-bottom: 1px solid hsla(0,0%,100%,.7);
+    }
+    .material-icons:active {
+		background-color: rgba(100, 100, 100, 0.3);
+    }
+	.close:hover {
+		border-bottom: 1px solid #fc3636;
+    }
+	.close:active {
+		background-color: rgba(255, 0, 0, 0.3);
+		border-bottom: 1px solid #fc3636;
     }
 </style>
